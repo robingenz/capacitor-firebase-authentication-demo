5273a925535679d68f5d65ba40773d7d3aaa8967
Robin Genz <mail@robingenz.dev>
2021-06-13 12:35:36 +0200

    refactor: update to Capacitor 3

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-firebase-authentication-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
