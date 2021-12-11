728e01cc05a42d66f5d75ed4865972419ba65cc2
Robin Genz <mail@robingenz.dev>
2021-12-11 22:07:57 +0100

    ci: add gh workflows and issue templates (#9)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { GlobalErrorHandlerService } from './core';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        {
-            provide: ErrorHandler,
-            useClass: GlobalErrorHandlerService,
-        },
-    ],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandlerService,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/login/login-routing.module.ts
+++ src/app/modules/login/login-routing.module.ts
@@ -6,8 +6,8 @@ import { LoginPage } from './login.page';
 const routes: Routes = [
   {
     path: '',
-    component: LoginPage
-  }
+    component: LoginPage,
+  },
 ];
 
 @NgModule({
